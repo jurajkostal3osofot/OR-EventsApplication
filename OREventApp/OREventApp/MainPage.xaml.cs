ff3257d8e2d98532c3a68cea156eeedc8afc5cfb
3OSOFT\juraj.kostal <juraj.kostal@3osoft.com>
2018-01-08 15:17:49 +0100

    Initial Commit

---
+++ OREventApp/OREventApp/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OREventApp.Pages;
+using Xamarin.Forms;
+
+namespace OREventApp
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+
+	    private void Cell_OnTapped(object sender, EventArgs e)
+	    {
+	        var page = new EventPickerPage();
+	        page.GetPickerEvents.ItemSelected += (src, args) =>
+	        {
+	            PickerEvents.Text = args.SelectedItem.ToString();
+
+                Navigation.PopAsync();
+	        };
+
+	        Navigation.PushAsync(page);
+        }
+	}
+}
---
+++ OREventApp/OREventApp/Pages/EventPickerPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace OREventApp.Pages
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class EventPickerPage : ContentPage
+	{
+	    public EventPickerPage()
+	    {
+	        InitializeComponent();
+	        PickerEventsList.ItemsSource = new List<string>
+	        {
+	            "None",
+	            "Football",
+	            "Florball"
+	        };
+
+	    }
+	    public ListView GetPickerEvents
+	    {
+	        get { return PickerEventsList; }
+	    }
+    }
+}
